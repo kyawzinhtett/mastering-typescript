37f0fcffe40a3a9a54581f1806174db42c5ae678
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-04-29 22:57:14 +0630

    feat: type aliases

--- src/object.ts
+++ src/object.ts
@@ -1,8 +1,10 @@
-const employee: {
+type Employee = {
     id: number,
     name: string
     retire: (date: Date) => void
-} = {
+}
+
+const employee: Employee = {
     id: 1,
     name: 'KZH',
     retire: (date: Date) => {
