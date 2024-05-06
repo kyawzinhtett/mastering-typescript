348de0d52d7de2fe151ba8900b89f3de2d3fcb6b
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2024-05-06 22:07:53 +0630

    feat: access control

--- dist/class.js
+++ dist/class.js
@@ -1,17 +1,21 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid Amount');
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Kyaw Zin Htet', 0);
 account.deposit(100);
 console.log(account instanceof Account);
+console.log(account.getBalance());
 //# sourceMappingURL=class.js.map
--- src/class.ts
+++ src/class.ts
@@ -1,21 +1,25 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string;
 
 
-    constructor (id: number, owner: string, balance: number) {
+    constructor (id: number, owner: string, _balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
 
     deposit (amount: number) {
         if (amount <= 0)
             throw new Error('Invalid Amount');
 
-        this.balance += amount;
+        this._balance += amount;
+    }
+
+    getBalance (): number {
+        return this._balance;
     }
 }
 
@@ -24,3 +28,8 @@ let account = new Account(1, 'Kyaw Zin Htet', 0);
 account.deposit(100);
 
 console.log(account instanceof Account);
+
+console.log(account.getBalance());
+
+
+ 
