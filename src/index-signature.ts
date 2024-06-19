d131328ee22e4247f7b482d197e3891323f4ea16
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2024-06-19 22:45:35 +0630

    feat: index signature

---
+++ src/index-signature.ts
@@ -0,0 +1,11 @@
+class SeatAssignment {
+    // A1, A2, ...
+    // Mosh, John, ...
+
+    // Index Signature
+    [seatNumber: string]: string;
+}
+
+let seats = new SeatAssignment();
+seats.A1 = 'Mosh';
+seats['A2'] = 'John';
