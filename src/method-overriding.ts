8393d1cc14941346e4678047d9521fb35d499be0
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2024-07-06 21:44:28 +0630

    feat: method overriding

---
+++ src/method-overriding.ts
@@ -0,0 +1,22 @@
+class People {
+    constructor (public firstName: string, public lastName: string) {}
+
+    get fullName () {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+    walk () {
+        console.log('Walking');
+    }
+}
+
+class Teacher extends People {
+    override get fullName () {
+        return 'Professor ' + super.fullName;
+    }
+}
+
+let teacher = new Teacher('John', 'Doe');
+
+console.log(teacher.fullName);
+
